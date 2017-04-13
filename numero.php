e5714e5226e694d75242c3b1dc516981a4770bd8
ZohraZaghbaniZ <zohrazaghbanizaghbani1@gmail.com>
2017-04-13 13:17:27 +0200

    Add files via upload

---
+++ numero.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Learn English</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<!--[if IE 6]><link rel="stylesheet" type="text/css" href="css/ie6.css"><![endif]-->
+</head>
+<body>
+<div id="mainPan">
+  <div id="leftPan">
+<div id="header">
+  <div> <a href="index.html"><img src="images/arabic.jpg" alt=""></a>
+  
+    <ul >
+      <li><a href="index.html"> الصفحةالرئسية </a></li>
+      <li><a href="parent.html">فضاء الولي</a></li>
+	  <li><a href="login.html">الدخول</a></li>
+	  <li><a href="signup.html">التسجيل</a></li>
+	  <li><a href="contact.html">تواصل</a></li>
+    </ul>
+	<br>
+	 </div>
+  </div>
+ </div>
+  </div>
+ <br><br>
+ <div class="w3-container">
+ <div class="row">
+<div class="w3-col s4">
+ <a href="n1.php"><div class="w3-card-4" style="width:80%">
+    <img src="./images/numero.jpg" alt="Norway" style="width:100%;height:25%;">
+    <div class="w3-container w3-center">
+      <h4>أرقام من 1 إلى 10</</h4>
+    </div>
+	
+	
+  
+</div></a>
+</div>
+<div class="w3-col s4">
+ <a href="n2.php"><div class="w3-card-4" style="width:80%">
+    <img src="./images/num.jpg" alt="Norway" style="width:100%;height:30%;">
+    <div class="w3-container w3-center">
+      <h4>أرقام من 11 إلى 20</</h4>
+    </div>
+  </div></a>
+  </div>
+  
+  
+  
+  <div class="w3-col s4">
+ <a href="n3.php"><div class="w3-card-4" style="width:80%">
+    <img src="./images/num2.jpg" alt="Norway" style="width:100%;height:30%;">
+    <div class="w3-container w3-center">
+      <h4>أرقام من 21 إلى 30</</h4>
+    </div>
+  </div></a>
+  </div>
+  
+  
+  
+  
+ </div>
+ <br><br>
+<div class="row"> 
+<div class="w3-col s4">
+ <a href="n4.php"><div class="w3-card-4" style="width:80%">
+    <img src="./images/num3.jpg" alt="Norway" style="width:100%;height:30%;">
+    <div class="w3-container w3-center">
+      <h4>أرقام من 31 إلى 40</</h4>
+    </div>
+  </div></a>
+  </div>
+   <div class="w3-col s4">
+ <a href="n5.php"><div class="w3-card-4" style="width:80%">
+    <img src="./images/num5.jpg" alt="Norway" style="width:100%;height:100%;">
+    <div class="w3-container w3-center">
+      <h4>أرقام من 41 إلى 50</</h4>
+    </div>
+  </div></a>
+  </div>
+  
+  </div>
+  </div>
+</div>
+</body>
+</html>
